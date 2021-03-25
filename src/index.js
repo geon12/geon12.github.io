46f18802354e427b431e4a92ecf39fb9b868c513
Cameron <camcreel@gmail.com>
2021-03-25 18:42:09 -0400

    Add interest arrays

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const bookList = [
+    {title: "The Code Breaker", author: "Walter Isaacson", url: "https://www.amazon.com/Code-Breaker-Jennifer-Doudna-Editing/dp/1982115858"},
+    {title:"Beginners", author: "Tom Vanderbilt", url: "https://www.amazon.com/Beginners-Transformative-Power-Lifelong-Learning/dp/1524732168"},
+    {title: "Dawn of the Belle Epoque", author: "Mary McAuliffe", url:"https://www.amazon.com/Dawn-Belle-Epoque-Bernhardt-Clemenceau/dp/1442209283/ref=sr_1_1?crid=1QMW267WGJ3WP&dchild=1&keywords=dawn+of+the+belle+epoque&qid=1616710230&s=books&sprefix=dawn+of+the+bell%2Cstripbooks%2C692&sr=1-1"}
+]
+
+const tvList = [
+    {title: "The Falcon and the Winter Soldier", url: "https://www.youtube.com/watch?v=IWBsDaFWyTE"},
+    {title: "Formula 1: Drive to Survive", url: "https://www.youtube.com/watch?v=GMrHKc58aIA"},
+    {title: "WandaVIsion", url: "https://www.youtube.com/watch?v=sj9J2ecsSpo"}
+]
+
+const listenList = [
+    {title: "History of the Twentieth Century Podcast", url: "https://historyofthetwentiethcentury.com/"},
+    {title: "Revolutions Podcast", url: "https://thehistoryofrome.typepad.com/revolutions_podcast/"},
+    {title: "Planet Money", url: "https://www.npr.org/sections/money/"}
+
+]
+
+
+function createInterestHTML() {
+
+}
